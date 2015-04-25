4eb0f5b24d3f3149716aaa21d22b79969c5061b1
Vivi Jiang <mobojiang@gmail.com>
2015-04-25 19:16:03 +0800

    add test case for number2

--- spec/prime/src/Prime.js
+++ spec/prime/src/Prime.js
@@ -3,9 +3,16 @@ Prime.isPrime = function(inputNumber) {
     /* body... */
 };
 Prime.getPrime = function(inputNumber) {
-    if (inputNumber === 1) {
-        return [1];
+	var result=[];
+    if (inputNumber === 1 || inputNumber === 2) {
+    	result.push(inputNumber);
+        return result;
     }
+   //  var canBeBreakdown = true;
+   // while(inputNumber/2 === 0)
+   // {
+
+   // }
 };
 
 module.exports = Prime;
--- spec/prime/test/primeSpec.js
+++ spec/prime/test/primeSpec.js
@@ -4,9 +4,14 @@ describe("Prime", function() {
     it('should return [1]', function() {
         var sourceNumber = 1;
         var result = Prime.getPrime(sourceNumber);
-        console.log(result);
         expect(result).toEqual([1]);
     })
 
+
+    it('should return [2]', function() {
+        var sourceNumber = 2;
+        var result = Prime.getPrime(sourceNumber);
+        expect(result).toEqual([2]);
+    })
 
 });
